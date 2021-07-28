ac76872e26f093a266df4cb98be2160bbe2fa7bd
Sergej <darkhorse0723@gmail.com>
2021-07-28 16:43:53 +0300

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Example Gastby Website",
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-gatsby-cloud"],
+};
